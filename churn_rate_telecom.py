c863c0cfb3ce6ec66dfbbd85e53c4eb81ebf5ed4
Kaushal0912 <kaushal@ucdconnect.ie>
2020-03-15 23:59:36 +0000

    Update churn_rate_telecom.py

--- churn_rate_telecom.py
+++ churn_rate_telecom.py
@@ -8,9 +8,16 @@ import copy
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import RFE
 
 dataset  = pd.read_excel('C:/Users/lenovo/Desktop/DOCUMENTS/AcmeTeleABT.xlsx') 
 dataset.drop(dataset.columns[-1],axis=1,inplace=True)
@@ -23,7 +30,7 @@ integer_encoded_children = label_encoder.fit_transform(dataset['children'])
 integer_encoded_credit = label_encoder.fit_transform(dataset['credit'])
 integer_encoded_creditCard = label_encoder.fit_transform(dataset['creditCard'])
 integer_encoded_marry = label_encoder.fit_transform(dataset['marry'])
-
+integer_encoded_churn = label_encoder.fit_transform(dataset['churn'])
 encoded_ds['children'] = integer_encoded_children
 encoded_ds['credit'] = integer_encoded_credit
 encoded_ds['creditCard'] = integer_encoded_creditCard
@@ -38,4 +45,25 @@ for col in encoded_ds.columns:
     print('\n')
 
  
+    
+    
+#build X and y
+X = encoded_ds.iloc[:,0:-1] #all columns except last column
+y = integer_encoded_churn 
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)    
+clf_knn = KNeighborsClassifier(n_neighbors=5)
+clf_rf = RandomForestClassifier(random_state=1, n_estimators=100, max_depth=100, max_leaf_nodes=100)
+clf_lr = LogisticRegression(class_weight="balanced")
+estimators = [('knn', clf_knn), ('lr', clf_lr), ('dt', clf_rf)]
+clf_avg = VotingClassifier(estimators,voting='soft')
+clf_avg.fit(X_train,y_train)
+print(accuracy_score(y_test, clf_avg.predict(X_test)))
+y_pred = [0, 2, 1, 3]
+y_true = [0, 1, 2, 3]
+print(accuracy_score(y_true, y_pred))
 
+
+plt.figure(figsize=(12,10))
+cor = encoded_ds.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+plt.show()
